552b42113213681e382532e5656fcf150007c30e
Jitgitbit <rowowolf@gmail.com>
2020-04-20 14:07:38 +0200

    shooter mobility done

--- app.js
+++ app.js
@@ -21,4 +21,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
   //draw the shooter
   squares[currentShooterIndex].classList.add('shooter')
+
+  //move the shooter along a line
+  function moveShooter(e) {
+    squares[currentShooterIndex].classList.remove('shooter')
+    switch(e.keyCode) {
+      case 37:
+        if(currentShooterIndex % width !== 0) currentShooterIndex -= 1
+        break
+      case 39:
+        if(currentShooterIndex % width < width - 1) currentShooterIndex += 1
+        break
+    }
+    squares[currentShooterIndex].classList.add('shooter')
+  }
+  document.addEventListener('keydown', moveShooter)
 })
