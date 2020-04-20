de0e087357606ce65153d4f60b08885b43c8c1eb
Jitgitbit <rowowolf@gmail.com>
2020-04-20 14:20:05 +0200

    shoot fn made

--- app.js
+++ app.js
@@ -83,4 +83,44 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
   invaderId = setInterval(moveInvaders, 500)
+
+  //shoot at aliens
+  function shoot(e) {
+    let laserId
+    let currentLaserIndex = currentShooterIndex
+    //move the laser from the shooter to the alien invader
+    function moveLaser() {
+      squares[currentLaserIndex].classList.remove('laser')
+      currentLaserIndex -= width
+      squares[currentLaserIndex].classList.add('laser')
+      if(squares[currentLaserIndex].classList.contains('invader')) {
+        squares[currentLaserIndex].classList.remove('laser')
+        squares[currentLaserIndex].classList.remove('invader')
+        squares[currentLaserIndex].classList.add('boom')
+
+        setTimeout(() => squares[currentLaserIndex].classList.remove('boom'), 250)
+        clearInterval(laserId)
+
+        const alienTakenDown = alienInvaders.indexOf(currentLaserIndex)
+        alienInvadersTakenDown.push(alienTakenDown)
+        result++
+        resultDisplay.textContent = result
+      }
+
+      if(currentLaserIndex < width) {
+        clearInterval(laserId)
+        setTimeout(() => squares[currentLaserIndex].classList.remove('laser'), 100)
+      }
+    }
+    // document.addEventListener('keyup', e => {
+    //   if (e.keyCode === 32) {
+    //     laserId = setInterval(moveLaser, 100)
+    //   }
+    // })
+    switch(e.keyCode) {
+      case 32:
+        laserId = setInterval(moveLaser, 100)
+        break
+    }
+  }
 })
