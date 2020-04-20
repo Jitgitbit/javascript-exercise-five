5a884d04bc1d59affca4da16653c9df7648d1ab0
Jitgitbit <rowowolf@gmail.com>
2020-04-20 14:04:08 +0200

    alien invaders drawn

--- app.js
+++ app.js
@@ -15,4 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
     15,16,17,18,19,20,21,22,23,24,
     30,31,32,33,34,35,36,37,38,39
   ]
+
+  //draw the alien invaders
+  alienInvaders.forEach( invader => squares[currentInvaderIndex + invader].classList.add('invader'))
 })
